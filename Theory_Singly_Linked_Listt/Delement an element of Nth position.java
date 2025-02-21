9997d6d2503f74aad4cbb1e6bef418e01ef5dca6
ayhanarashtasin <148694751+ayhanarashtasin@users.noreply.github.com>
2025-02-21 14:15:41 +0600

    Delement an element of Nth position.java

--- Theory_Singly_Linked_Listt/Delement an element of Nth position.java
+++ Theory_Singly_Linked_Listt/Delement an element of Nth position.java
@@ -1,11 +1,15 @@
 public static void DeleteNthPosition(ListNode head,int pos){
+        if (pos<0){
+            printLinkedList(head);
+            return;
+        }
         if(pos==0){
             head = head.next;
             printLinkedList(head);
             return;
         }
         ListNode temp = head;
-        for(int i=0; i<pos-2; i++){
+        for(int i=0; i<pos-1; i++){
             temp = temp.next;
         }
         temp.next = temp.next.next;
