5f5376955d25c2f832b73e6f57a268ff4b1c56ea
ayhanarashtasin <148694751+ayhanarashtasin@users.noreply.github.com>
2025-02-26 12:11:07 +0600

    Task 02: Container with Most Water.java

---
+++ Lab/Lab-01/Task 02: Container with Most Water.java
@@ -0,0 +1,34 @@
+class Task2{
+    //Complete this method so that it gives the Expected Output
+    public static void mostWater( Integer[] height ){
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+            maxArea = Math.max(maxArea, (right - left) * Math.min(height[left], height[right]));
+
+            if (height[left] < height[right]) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+
+        System.out.println(maxArea);
+
+    }
+
+    //DO NOT CHANGE ANY DRIVER CODE BELOW THIS LINE
+    public static void main(String[] args){
+        Integer[] array = {1,8,6,2,5,4,8,3,7};
+        System.out.println("Given Array: ");
+        Arr.print(array);
+        System.out.println("\nExpected Output:");
+        System.out.print("49");
+	System.out.print("\nYour Output:\n");
+        mostWater( array );
+
+    }
+}
