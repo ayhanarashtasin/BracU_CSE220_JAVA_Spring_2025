dacb883dabf4226b9ea4a61851d96087f1006dad
ayhanarashtasin <148694751+ayhanarashtasin@users.noreply.github.com>
2025-03-03 00:12:02 +0600

    Create 234. Palindrome Linked List.java

---
+++ Theory_Singly_Linked_Listt/234. Palindrome Linked List.java
@@ -0,0 +1,32 @@
+class Solution {
+    public ListNode reverse(ListNode head){
+        ListNode temp=head;
+        ListNode previous = null;
+        while(temp!=null){
+            ListNode front = temp.next;
+            temp.next = previous;
+            previous = temp;
+            temp = front;
+        }
+        return previous;
+    }
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode rev = reverse(slow.next);
+        ListNode first = head;
+        ListNode second = rev;
+        while(second!=null){
+            if(first.val != second.val){
+                return false;
+            }
+            first = first.next;
+            second = second.next;
+        }
+        return true;
+    }
+}
