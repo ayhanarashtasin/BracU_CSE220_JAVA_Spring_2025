79b74fc1933d38364c311368b9bce307b637dbbb
ayhanarashtasin <148694751+ayhanarashtasin@users.noreply.github.com>
2025-03-08 19:59:33 +0600

    Update Task 01 - Zigzag Walk.java

--- Lab/Lab-03-2D Matrix/Task 01 - Zigzag Walk.java
+++ Lab/Lab-03-2D Matrix/Task 01 - Zigzag Walk.java
@@ -1,25 +1,31 @@
 import java.util.Arrays;
-public class Main {
-  public static void zigzagwalk(int[][] Matrix){
-    int row = Matrix.length;
-    int col = Matrix[0].length;
-    int top = 0;
-    int bottom = row-1;
-    int left = 0;
-    while(left<col){
-      if(left%2==0){
-        for(int i=top;i<=bottom;i+=2){
-          System.out.print(Matrix[i][left]+" ");
+public class ZigzagWalk {
+    public static void walkZigzag(int[][] matrix) {
+        int row = matrix.length;        // Number of rows
+        int col = matrix[0].length;     // Number of columns
+        int top = 0;
+        int bottom = row - 1;
+        int left = 0;
+
+        while (left < col) {
+            if (left % 2 == 0) {
+                for (int i = top; i <= bottom; i += 2) {
+                    System.out.print(matrix[i][left] + " ");
+                }
+                System.out.println();
+                left++;
+            } else {
+                if (row % 2 != 0) {
+                    for (int i = bottom - 1; i >= top; i -= 2) {
+                        System.out.print(matrix[i][left] + " ");
+                    }
+                } else {
+                    for (int i = bottom; i >= top; i -= 2) {
+                        System.out.print(matrix[i][left] + " ");
+                    }
+                }
+                System.out.println();
+                left++;
+            }
         }
-        System.out.println(" ");
-        left+=1;
-      }
-      else{
-        for(int i=bottom-1; i>top;i-=2){
-          System.out.print(Matrix[i][left]+" ");
-        }
-        System.out.println(" ");
-        left+=1;
-      }
     }
-  }
