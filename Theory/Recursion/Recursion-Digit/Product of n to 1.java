d738f4d231d50ffb2f280e195bb23fa4207b059f
ayhanarashtasin <148694751+ayhanarashtasin@users.noreply.github.com>
2025-04-08 20:23:25 +0600

    Create Product of n to 1.java

---
+++ Theory/Recursion/Recursion-Digit/Product of n to 1.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(product(5));
+    }
+    static int product(int n){
+        if(n==1){
+            return 1;
+        }
+        return n*product(n-1);
+    }
+}
