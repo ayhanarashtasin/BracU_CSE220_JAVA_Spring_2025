29795d0739c1f065527885edd8b64c26d94ca7a3
ayhanarashtasin <148694751+ayhanarashtasin@users.noreply.github.com>
2025-04-19 19:37:48 +0600

    Update Task 02 - Level Min.java

--- Lab/Lab-06-Binary Tree/Task 02 - Level Min.java
+++ Lab/Lab-06-Binary Tree/Task 02 - Level Min.java
@@ -1,29 +1,35 @@
-public static void convertMirror(BTNode root ){
-        Queue<Node> q = new LinkedList<>();
-        q.add(root);
-        q.add(null);
-        int minimum = Integer.MAX_VALUE;
-        while(!q.isEmpty()){
-            BTNode currentNode = q.remove();
-            if(currentNode==null){
-                System.out.println(minimum);
-                minimum = Integer.MAX_VALUE;
-                if(q.isEmpty()){
-                    break;
-                }
-                else{
-                    q.add(null);
-                }
-            }
-            else{
-                minimum = Math.min(minimum,currentNode.elem);
+public static void smallestLevel(BTNode root, Integer[] levelArray, int lvl) {
+    Queue<BTNode> q = new LinkedList<>();
+    q.add(root);
+    q.add(null);
+    int minimum = Integer.MAX_VALUE;
+    int level = lvl;
 
-                if(currentNode.left !=null){
-                    q.add(currentNode.left);
-                }
-                if(currentNode.right !=null){
-                    q.add(currentNode.right);
-                }
+    while (!q.isEmpty()) {
+        BTNode currentNode = q.remove();
+        if (currentNode == null) {
+            levelArray[level] = minimum;
+            minimum = Integer.MAX_VALUE;
+            level++;
+            if (q.isEmpty()) {
+                break;
+            } else {
+                q.add(null);
+            }
+        } else {
+            minimum = Math.min(minimum, currentNode.elem);
+
+            if (currentNode.left != null) {
+                q.add(currentNode.left);
+            }
+            if (currentNode.right != null) {
+                q.add(currentNode.right);
             }
         }
     }
+    for (int i = 0; i < level; i++) {
+        System.out.print(levelArray[i] + " ");
+    }
+    System.out.println();
+}
+
