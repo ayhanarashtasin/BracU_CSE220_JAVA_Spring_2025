6612c0366e168c045e9699387555d6f7567e7e3f
ayhanarashtasin <148694751+ayhanarashtasin@users.noreply.github.com>
2025-05-24 20:28:13 +0600

    Create UpperTriangle.java

---
+++ Theory/2D Array/UpperTriangle.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Matrix {
+    public static int MatrixUpperTriangle(int[][] matrix, int target) {
+        int n=matrix.length;
+        int uppertriangle=0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<=i;j++){
+                uppertriangle+=matrix[i][j];
+            }
+        }
+        return uppertriangle;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {3,  5,   7},
+            {10, 11, 16},
+            {20, 23, 30}
+        };
+        int target = 16;
+        System.out.println(MatrixUpperTriangle(matrix, target));
+    }
+}
