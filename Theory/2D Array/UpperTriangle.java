8a058ec7578b17161385f753dd6c980723f9538d
ayhanarashtasin <148694751+ayhanarashtasin@users.noreply.github.com>
2025-05-25 11:46:20 +0600

    Update UpperTriangle.java

--- Theory/2D Array/UpperTriangle.java
+++ Theory/2D Array/UpperTriangle.java
@@ -4,9 +4,9 @@ public class Matrix {
     public static int MatrixUpperTriangle(int[][] matrix) {
         int n=matrix.length;
         int uppertriangle=0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<=i;j++){
-                uppertriangle+=matrix[i][j];
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                uppertriangle += matrix[i][j];
             }
         }
         return uppertriangle;
