77a6b924b538ae1faef3e648f2dd7f827c47ea99
ayhanarashtasin <148694751+ayhanarashtasin@users.noreply.github.com>
2025-07-25 02:13:15 +0600

    Update task-07.java

--- Lab/Doubly Linked List/task-07.java
+++ Lab/Doubly Linked List/task-07.java
@@ -1,1 +1,23 @@
+public void rangeMove(Node head, int start, int end) {
+        int size = 0;
+        Node counter = head.next;
+        while (counter != head) {
+            size++;
+            counter = counter.next;
+        }
+        Node current = head.next;
+        for (int i = 0; i < size; i++) {
+            Node nextNodeToProcess = current.next;
+            if (current.data >= start && current.data <= end) {
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
 
+                Node tail = head.prev;
+                tail.next = current;
+                current.prev = tail;
+                current.next = head;
+                head.prev = current;
+            }
+            current = nextNodeToProcess;
+        }
+}
