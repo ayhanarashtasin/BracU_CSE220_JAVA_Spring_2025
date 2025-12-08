13b175d049f5dc209ac074765ab82904c24f953f
ayhanarashtasin <148694751+ayhanarashtasin@users.noreply.github.com>
2025-12-08 21:10:39 +0600

    Update Substraction.java

--- Theory/Recursion/Recursion -LinkedList/Substraction.java
+++ Theory/Recursion/Recursion -LinkedList/Substraction.java
@@ -1,12 +1,12 @@
-static int subtraction(Node head, int index,int OddSum ,int Even_Multiplication){
+static int subtraction(Node head, int OddSum ,int Even_Multiplication){
     if(head==null){
-      return Even_Multiplication-OddSum;
+      return abs(Even_Multiplication-OddSum);
     }
-    if(index%2==0){
-      return subtraction(head.next,index+1,OddSum,Even_Multiplication*head.value);
+    if(head.value%2==0){
+      return subtraction(head.next,OddSum,Even_Multiplication*head.value);
     }
-    if(index%2!=0){
-      return subtraction(head.next,index+1,OddSum+head.value,Even_Multiplication);
+    if(head.value%2!=0){
+      return subtraction(head.next,OddSum+head.value,Even_Multiplication);
     }
     return 0;
   }
