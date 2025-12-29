31db54dfb3f63fd30b7c2bddb9c5714b7b78d0d9
ayhanarashtasin <148694751+ayhanarashtasin@users.noreply.github.com>
2025-12-30 00:03:04 +0600

    Fix heapify call in HeapSort.java

--- Theory/Heap & PriorityQueue/HeapSort.java
+++ Theory/Heap & PriorityQueue/HeapSort.java
@@ -14,7 +14,7 @@ public class Main {
       int temp=arr[i];
       arr[i]=arr[largest];
       arr[largest]=temp;
-      heapify(arr,i,0);
+      heapify(arr,largest,0);
     }
   }
   public static void heapsort(int[] arr){
